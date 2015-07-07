6e87b4a2f79be1222f3e62069e02086b91f965fb
Noam Amisar <noam@weka.io>
2015-07-07 16:51:48 +0300

    Added support for line timeout in popen.iter_lines

--- plumbum/commands/processes.py
+++ plumbum/commands/processes.py
@@ -75,7 +75,8 @@ def _check_process(proc, retcode, timeout, stdout, stderr):
                 stdout, stderr, machine)
     return proc.returncode, stdout, stderr
 
-def _iter_lines(proc, decode, linesize):
+def _iter_lines(proc, decode, linesize, line_timeout=None):
+
     try:
         from selectors import DefaultSelector, EVENT_READ
     except ImportError:
@@ -83,7 +84,9 @@ def _iter_lines(proc, decode, linesize):
         from select import select
         def selector():
             while True:
-                rlist, _, _ = select([proc.stdout, proc.stderr], [], [])
+                rlist, _, _ = select([proc.stdout, proc.stderr], [], [], line_timeout)
+                if not rlist and line_timeout:
+                    raise ProcessLineTimedOut("popen line timeout expired", getattr(proc, "argv", None), getattr(proc, "machine", None))
                 for stream in rlist:
                     yield (stream is proc.stderr), decode(stream.readline(linesize))
     else:
@@ -93,7 +96,10 @@ def _iter_lines(proc, decode, linesize):
             sel.register(proc.stdout, EVENT_READ, 0)
             sel.register(proc.stderr, EVENT_READ, 1)
             while True:
-                for key, mask in sel.select():
+                ready = sel.select(line_timeout)
+                if not ready and line_timeout:
+                    raise ProcessLineTimedOut("popen line timeout expired", getattr(proc, "argv", None), getattr(proc, "machine", None))
+                for key, mask in ready:
                     yield key.data, decode(key.fileobj.readline(linesize))
 
     for ret in selector():
@@ -147,6 +153,16 @@ class ProcessTimedOut(Exception):
         Exception.__init__(self, msg, argv, machine)
         self.argv = argv
         self.machine = machine
+
+
+class ProcessLineTimedOut(Exception):
+    """Raises by :func:`iter_lines <plumbum.commands.iter_lines>` when a ``line_timeout`` has been
+    specified and it has elapsed before the process yielded another line"""
+    def __init__(self, msg, argv, machine):
+        Exception.__init__(self, msg, argv, machine)
+        self.argv = argv
+        self.machine = machine
+
 
 class CommandNotFound(Exception):
     """Raised by :func:`local.which <plumbum.machines.local.LocalMachine.which>` and
@@ -268,7 +284,7 @@ def run_proc(proc, retcode, timeout = None):
 #===================================================================================================
 # iter_lines
 #===================================================================================================
-def iter_lines(proc, retcode = 0, timeout = None, linesize = -1, _iter_lines = _iter_lines):
+def iter_lines(proc, retcode = 0, timeout = None, linesize = -1, _iter_lines = _iter_lines, line_timeout=None):
     """Runs the given process (equivalent to run_proc()) and yields a tuples of (out, err) line pairs.
     If the exit code of the process does not match the expected one, :class:`ProcessExecutionError
     <plumbum.commands.ProcessExecutionError>` is raised.
@@ -296,7 +312,7 @@ def iter_lines(proc, retcode = 0, timeout = None, linesize = -1, _iter_lines = _
     _register_proc_timeout(proc, timeout)
 
     buffers = [[], []]
-    for t, line in _iter_lines(proc, decode, linesize):
+    for t, line in _iter_lines(proc, decode, linesize, line_timeout):
         ret = [None, None]
         ret[t] = line
         buffers[t].append(line)
